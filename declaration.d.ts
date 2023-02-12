3d249e0d2e307a4fc974d8f006fdc7a95d0c2cc8
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-02-12 16:04:33 +0800

    ⬆️

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-import 'style9';
-
-declare module 'style9' {
-  interface CustomProperties {
-    WebkitFontSmoothing?: string;
-    MozOsxFontSmoothing?: string;
-  }
-}
